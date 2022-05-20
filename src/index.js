f8517e02b71c350f06225f7b2e97ae36557d420c
six <quocsy2511@gmail.com>
2022-05-20 10:57:50 +0700

    learnNodeJs

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require ('express-handlebars');
+const { extname } = require('path');
+const app = express()
+const port = 3000
+
+//
+app.use(express.static(path.join(__dirname, 'public')));
+//HTTP loger
+app.use(morgan('combined'));
+//template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
